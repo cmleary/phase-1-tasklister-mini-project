4bccaf67583605500ea46d528b3e64bffbceb912
Chris Leary <learymchris@gmail.com>
2023-08-02 16:40:44 -0400

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const newTask = document.getElementById('create-task-form')
+  newTask.addEventListener("submit", (event) => {
+      event.preventDefault()
+
+      const task = document.getElementById('new-task-description').value;
+      const entry = document.createElement('li');
+      entry.appendChild(document.createTextNode(task));
+      list.appendChild(entry);
+      newTask.reset()
+  })
+
+
+})
+
+
+
+//dro down to change text color
